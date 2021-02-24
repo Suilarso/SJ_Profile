53966498fa11736f73db0ff3b9d55383eb858d4e
SuilarsoJapit <suilarso.japit@skipthedishes.ca>
2021-02-24 15:47:28 -0600

    Change Router to BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,37 +1,18 @@
-import React from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
-import { Container } from 'react-bootstrap'
-import Header from './components/Header'
+import React from 'react';
+// import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+import Header from './components/Header';
 // import HomeScreen from './screens/HomeScreen'
 // import LoginScreen from './screens/LoginScreen'
 // import RegisterScreen from './screens/RegisterScreen'
 // import ProfileScreen from './screens/ProfileScreen'
 
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
+//<img src={logo} className="App-logo" alt="logo" />
 function App() {
   return (
-    <Router>
+    <BrowserRouter>
       <Header />
       {/* <main className='py-5'>
         <Container>
@@ -43,7 +24,7 @@ function App() {
           <Route path='/items' component={ItemMgmtScreen} exact />
         </Container>
       </main> */}
-    </Router>
+    </BrowserRouter>
   )
 }
 
