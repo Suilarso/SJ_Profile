342ef166781b70b994c20fb90b85247ecb3cfdb8
Suilarso <suilarso@gmail.com>
2021-02-26 14:30:38 -0600

    uncomment constant

--- src/reducers/userReducers.js
+++ src/reducers/userReducers.js
@@ -24,7 +24,7 @@ export function userLoginReducer(state = {}, action)
     case USER_LOGIN_REQUEST:
       return { loading: true };
     case USER_LOGIN_SUCCESS:
-      return { loading: false, userAuth: action.payload };
+      return { loading: false, userToken: action.payload };
     case USER_LOGIN_FAIL:
       return { loading: false, error: action.payload };
     case USER_LOGOUT:
