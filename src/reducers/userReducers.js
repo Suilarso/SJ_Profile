168d962292b0694a8770c7210352ed8307311e15
Suilarso <suilarso@gmail.com>
2021-03-08 19:22:21 -0600

    Un-comment userDetailsReducer and userUpdateProfileReducer functions

--- src/reducers/userReducers.js
+++ src/reducers/userReducers.js
@@ -55,32 +55,36 @@ export function userRegisterReducer(state = {}, action)
   }
 }
 
-// export function userDetailsReducer(state = {}, action) {
-//   switch (action.type) {
-//     case USER_DETAILS_REQUEST:
-//       return { ...state, loading: true }
-//     case USER_DETAILS_SUCCESS:
-//       return { loading: false, userInfo: action.payload }
-//     case USER_DETAILS_FAIL:
-//       return { loading: false, error: action.payload }
-//     case USER_DETAILS_RESET:
-//       return { user: {} }
-//     default:
-//       return state
-//   }
-// }
+export function userDetailsReducer(state = {}, action) {
 
-// export function userUpdateProfileReducer(state = {}, action) {
-//   switch (action.type) {
-//     case USER_UPDATE_PROFILE_REQUEST:
-//       return { loading: true }
-//     case USER_UPDATE_PROFILE_SUCCESS:
-//       return { loading: false, success: true }
-//     case USER_UPDATE_PROFILE_FAIL:
-//       return { loading: false, failure: action.payload }
-//     case USER_UPDATE_PROFILE_RESET:
-//       return {}
-//     default:
-//       return state
-//   }
-// }
+  switch (action.type) {
+    case USER_DETAILS_REQUEST:
+      return { ...state, loading: true }
+    case USER_DETAILS_SUCCESS:
+      return { loading: false, userInfo: action.payload }
+    case USER_DETAILS_FAIL:
+      return { loading: false, error: action.payload }
+    case USER_DETAILS_RESET:
+      return { user: {} }
+    default:
+      return state;
+  }
+
+}
+
+export function userUpdateProfileReducer(state = {}, action) {
+
+  switch (action.type) {
+    case USER_UPDATE_PROFILE_REQUEST:
+      return { loading: true }
+    case USER_UPDATE_PROFILE_SUCCESS:
+      return { loading: false, success: true }
+    case USER_UPDATE_PROFILE_FAIL:
+      return { loading: false, failure: action.payload }
+    case USER_UPDATE_PROFILE_RESET:
+      return {}
+    default:
+      return state;
+  }
+
+}
